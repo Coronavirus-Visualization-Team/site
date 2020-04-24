4140aa62ebd6fb29f22c7e18af121ca46b926936
Adway Wadekar <adway@adway.io>
2020-04-23 20:56:56 -0400

    Add a working theme!

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+/** @jsx jsx */
+import React from "react"
+import theme from "../gatsby-plugin-theme-ui"
+import { Styled, jsx } from "theme-ui"
+
+export default () => (
+  <>
+    <h1>Hello, world!</h1>
+  </>
+)
