0f3ddd694dc0d416c8c08378c5148f327613f550
Steve Li <lithafnium2000@gmail.com>
2020-04-26 01:03:41 -0400

    added queries

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,51 +1,91 @@
 /** @jsx jsx */
-import React from "react"
+import React, { useEffect } from "react"
 import theme from "../gatsby-plugin-theme-ui"
+import PropTypes from 'prop-types'
+import { graphql, StaticQuery } from 'gatsby'
 import { Container, Styled, jsx, Text, Divider, Grid } from "theme-ui"
 import ProjectPicker from "../components/Projects/ProjectPicker"
 import Tile from "../components/Projects/Tile"
 
-export default () => (
-  <>
-    <Container
-      sx={{
-        width: "100%",
-        maxWidth: "xl",
-        mb: ["5em", "15em"],
-      }}
-    >
+const Projects = ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark
+  
+  return (
+    <>
       <Container
         sx={{
-          fontSize: [2, 3],
-          position: "relative",
-          bg: "#168CA6",
+          width: "100%",
           maxWidth: "xl",
-          p: 4,
-          borderRadius: "project",
-          height: ["fit-content", "500px"],
-          boxShadow: "base",
+          mb: ["5em", "15em"],
         }}
       >
-        <Text
+        <Container
           sx={{
-            mb: "2rem",
-            fontWeight: "700",
+            fontSize: [2, 3],
+            position: "relative",
+            bg: "#168CA6",
+            maxWidth: "xl",
+            p: 4,
+            borderRadius: "project",
+            height: ["fit-content", "500px"],
+            boxShadow: "base",
           }}
         >
-          Here are some of the featured work we're doing.
+          <Text
+            sx={{
+              mb: "2rem",
+              fontWeight: "700",
+            }}
+          >
+            Here are some of the featured work we're doing.
         </Text>
-        <ProjectPicker />
+          <ProjectPicker />
+        </Container>
       </Container>
-    </Container>
-    <Container
-      sx={{
-        maxWidth: "xl",
-      }}
-    >
-      <Grid width={["45%"]} gap={["10%"]}>
-        <Tile />
-        <Tile />
-      </Grid>
-    </Container>
-  </>
+      <Container
+        sx={{
+          maxWidth: "xl",
+        }}
+      >
+        {posts && 
+        <Grid columns = {[2]} gap={[50, 50]}>
+          {posts.map(({node: post}) => {
+            return( <Tile title = {post.frontmatter.title}/>)
+          })}
+        </Grid>}
+      </Container>
+    </>
+  )
+}
+
+Projects.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+};
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query ProjectQuery {
+        allMarkdownRemark{
+          edges {
+            node {
+              excerpt(pruneLength: 400)
+              id
+              frontmatter {
+                path
+                title
+                featured
+                date(formatString: "MMMM DD, YYYY")
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={(data, count) => <Projects data={data} count={count} />}
+  />
 )
