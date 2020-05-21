acefca050a001402326a3b62baef0d85ea1518c3
Calix Huang <calix.huang1@gmail.com>
2020-05-20 21:58:01 -0700

    added default name in project slider

--- src/components/Projects/ProjectPicker.js
+++ src/components/Projects/ProjectPicker.js
@@ -14,6 +14,8 @@ const ProjectPicker = ({ posts }) => {
     const [three, toggleThree] = useState(false)
     const [four, toggleFour] = useState(false)
 
+    console.log(featured)
+
     return (
         <Container
             sx={{
@@ -38,48 +40,47 @@ const ProjectPicker = ({ posts }) => {
                             toggleThree(false)
                             toggleFour(false)
                         }}
-                        sx={{ variant: one ? 'styles.projectPickerActive' : 'styles.projectPicker' }}>
-                        Project 1</Box>
-                    <Box 
+                        sx={{ variant: one ? 'styles.projectPickerActive' : 'styles.projectPicker' }}>{featured[0] ? featured[0].node.frontmatter.title : "Coming Soon!"}</Box>
+                    <Box
                         onClick={() => {
                             toggleOne(false)
                             toggleTwo(true)
                             toggleThree(false)
                             toggleFour(false)
                         }}
-                        sx={{ variant: two ? 'styles.projectPickerActive' : 'styles.projectPicker' }}>Project 2</Box>
-                    <Box 
+                        sx={{ variant: two ? 'styles.projectPickerActive' : 'styles.projectPicker' }}>{featured[1] ? featured[1].node.frontmatter.title : "Coming Soon!"}</Box>
+                    <Box
                         onClick={() => {
                             toggleOne(false)
                             toggleTwo(false)
                             toggleThree(true)
                             toggleFour(false)
                         }}
-                        sx={{ variant: three ? 'styles.projectPickerActive' : 'styles.projectPicker' }}>Project 3</Box>
-                    <Box 
+                        sx={{ variant: three ? 'styles.projectPickerActive' : 'styles.projectPicker' }}>{featured[2] ? featured[2].node.frontmatter.title : "Coming Soon!"}</Box>
+                    <Box
                         onClick={() => {
                             toggleOne(false)
                             toggleTwo(false)
                             toggleThree(false)
                             toggleFour(true)
                         }}
-                        sx={{ variant: four ? 'styles.projectPickerActive' : 'styles.projectPicker' }}>Project 4</Box>
+                        sx={{ variant: four ? 'styles.projectPickerActive' : 'styles.projectPicker' }}>{featured[3] ? featured[3].node.frontmatter.title : "Coming Soon!"}</Box>
                 </Grid>
             </Container>
-            {one && <FeaturedProject 
-                        img={featured[0] ? featured[0].node.frontmatter.image : CoronaImg} 
+            {one && <FeaturedProject
+                        img={featured[0] ? featured[0].node.frontmatter.image : CoronaImg}
                         slug={featured[0] ? featured[0].node.frontmatter.slug : ''}
                         present={true && featured[0]}/>}
-            {two && <FeaturedProject 
+            {two && <FeaturedProject
                         img={featured[1] ? featured[1].node.frontmatter.image : CoronaImg }
                         slug={featured[1] ? featured[1].node.frontmatter.slug : ''}
                         present={true && featured[1]} />}
-            {three && <FeaturedProject 
-                        img={featured[2] ? featured[2].node.frontmatter.image : CoronaImg} 
+            {three && <FeaturedProject
+                        img={featured[2] ? featured[2].node.frontmatter.image : CoronaImg}
                         slug={featured[2] ? featured[2].node.frontmatter.slug : ''}
                         present={true && featured[2]}/>}
-            {four && <FeaturedProject 
-                        img={featured[3] ? featured[3].node.frontmatter.image : CoronaImg} 
+            {four && <FeaturedProject
+                        img={featured[3] ? featured[3].node.frontmatter.image : CoronaImg}
                         slug={featured[3] ? featured[3].node.frontmatter.slug : ''}
                         present={true && featured[3]}/>}
 
