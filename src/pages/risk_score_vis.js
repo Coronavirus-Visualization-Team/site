ef851ea7635eb96d214cf5b9c3ea9b92b1eaad0c
Wanyu Guan <wanyu20@uw.edu>
2020-07-03 20:35:41 -0700

    Create page for risk score and add risk score vis html

---
+++ src/pages/risk_score_vis.js
@@ -0,0 +1,14 @@
+/** @jsx jsx */
+import React from "react"
+import { graphql } from "gatsby"
+import { Box, Button, Grid, Image, jsx, Text, useColorMode } from "theme-ui"
+import Iframe from 'react-iframe'
+
+
+<Iframe url=" "
+        width="450px"
+        height="450px"
+        id="myId"
+        className="myClassname"
+        display="initial"
+        position="relative"/>
